de993a0d0ac822ca7bf70ac8c76a7de055dda4bb
vcosmin2701 <cosmin.vladut@ulbsibiu.ro>
2023-05-16 23:20:14 +0300

    Insert pawns on the chess board

--- src/components/ChessBoard.jsx
+++ src/components/ChessBoard.jsx
@@ -1,4 +1,20 @@
 import '../styles/ChessBoard.css'
+import '../styles/Piece.css'
+import dark_pawn from '../assets/chess-pieces/dark-pieces/Chess_pdt60.png';
+import dark_king from '../assets/chess-pieces/dark-pieces/Chess_kdt60.png';
+import dark_knight from '../assets/chess-pieces/dark-pieces/Chess_ndt60.png';
+import dark_queen from '../assets/chess-pieces/dark-pieces/Chess_qdt60.png';
+import dark_rook from '../assets/chess-pieces/dark-pieces/Chess_rdt60.png';
+import dark_bishop from '../assets/chess-pieces/dark-pieces/Chess_bdt60.png';
+
+import light_pawn from '../assets/chess-pieces/light-pieces/Chess_plt60.png';
+import light_king from '../assets/chess-pieces/light-pieces/Chess_klt60.png';
+import light_knight from '../assets/chess-pieces/light-pieces/Chess_nlt60.png';
+import light_queen from '../assets/chess-pieces/light-pieces/Chess_qlt60.png';
+import light_rook from '../assets/chess-pieces/light-pieces/Chess_rlt60.png';
+import light_bishop from '../assets/chess-pieces/light-pieces/Chess_blt60.png';
+
+
 const ChessBoard = () => {
     const verticalLines = ['1', '2', '3', '4', '5', '6', '7', '8'];
     const horizontalLines = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
@@ -10,13 +26,19 @@ const ChessBoard = () => {
             if(number % 2 === 0){
                 board.push(
                     <div className="tile black-tile">
-                        [{horizontalLines[i]}{verticalLines[j]}]
+                        <div className="pawns">
+                            {(j == 1)  && <div className="piece"><img src={light_pawn} alt='dark pawn' /></div>} {/* fill with light pawns */}
+                            {(j == 6)  && <div className="piece"><img src={dark_pawn} alt='dark pawn' /></div>} {/* fill with dark pawns */}
+                        </div>
                     </div>
                 );
             } else {
                 board.push(
                     <div className="tile white-tile">
-                        [{horizontalLines[i]}{verticalLines[j]}]
+                        <div className="pawns">
+                            {(j == 1)  && <div className="piece"><img src={light_pawn} alt='dark pawn' /></div>} {/* fill with light pawns */}
+                            {(j == 6)  && <div className="piece"><img src={dark_pawn} alt='dark pawn' /></div>} {/* fill with dark pawns */}
+                        </div>
                     </div>
                 );
             }
